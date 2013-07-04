d90c6eeae4b5c094c22b9a2f021135f6d7194348
Takuya Tominaga <taku@tomiflu.com>
2013-07-05 00:00:22 +0900

    音楽再生用クラスを作成

---
+++ Classes/CCPlaySE.h
@@ -0,0 +1,31 @@
+//
+//  CCPlaySE.h
+//  nyangame
+//
+//  Created by tomi on 7/4/13.
+//
+//
+
+#ifndef __nyangame__CCPlaySE__
+#define __nyangame__CCPlaySE__
+
+#include <iostream>
+#include "cocos2d.h"
+
+class CCPlaySE : public cocos2d::CCActionInstant {
+public:
+    CCPlaySE(std::string sound);
+    virtual ~CCPlaySE(){}
+    
+    virtual void update(float time);
+    virtual CCFiniteTimeAction* reverse(void);
+    virtual CCObject* copyWithZone(cocos2d::CCZone* pZone);
+    
+public:
+    static CCPlaySE* create(std::string sound);
+    
+protected:
+    std::string m_sound;
+};
+
+#endif /* defined(__nyangame__CCPlaySE__) */
